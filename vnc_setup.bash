93b5484c6598a0971727e5c935c7e38c9032b6c4
sokacza <sokacza@mcmaster.ca>
2023-03-18 12:07:39 -0400

    vncscript

--- vnc_setup.bash
+++ vnc_setup.bash
@@ -6,4 +6,10 @@ echo "PLEASE SET YOUR VNC ACCESS CREDENTIALS"
 gsettings set org.gnome.Vino authentication-methods "['vnc']"
 gsettings set org.gnome.Vino vnc-password $(echo -n '9055259140'|base64)
 vncserver
+mkdir -p ~/.config/autostart
+touch ~/.config/autostart/vnc2.bash
+echo -e "#! /bin/sh \nnvncserver :2 -depth 24 -geometry 1920x1080" > ~/.config/autostart/vnc2.bash
+chmod +x ~/.config/autostart/vnc2.bash
+sudo wget --output-document=/usr/share/backgrounds/MacBotWallpaper.png https://github.com/sokacza/macbot/raw/main/docs/Labs/Wallpapers/MARCWallpaper.png
+sudo reboot
 
