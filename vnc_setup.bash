d8d896b1a27c1c5e1319229d9ebd82cfc5cdec96
sokacza <sokacza@mcmaster.ca>
2023-03-18 12:09:05 -0400

    autostart

--- vnc_setup.bash
+++ vnc_setup.bash
@@ -7,9 +7,10 @@ gsettings set org.gnome.Vino authentication-methods "['vnc']"
 gsettings set org.gnome.Vino vnc-password $(echo -n '9055259140'|base64)
 vncserver
 mkdir -p ~/.config/autostart
-touch ~/.config/autostart/vnc2.bash
-echo -e "#! /bin/sh \nnvncserver :2 -depth 24 -geometry 1920x1080" > ~/.config/autostart/vnc2.bash
-chmod +x ~/.config/autostart/vnc2.bash
+touch ~/.config/autostart/autostart_vnc.bash
+echo -e "#! /bin/sh \nnvncserver :2 -depth 24 -geometry 1920x1080" > ~/.config/autostart/autostart_vnc.bash
+chmod +x ~/.config/autostart/autostart_vnc.bash
+ln ~/.config/autostart/autostart_vnc.bash ~
 sudo wget --output-document=/usr/share/backgrounds/MacBotWallpaper.png https://github.com/sokacza/macbot/raw/main/docs/Labs/Wallpapers/MARCWallpaper.png
 sudo reboot
 
